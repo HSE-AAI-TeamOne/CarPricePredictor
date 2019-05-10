bb2c07217fab8dee90dc92c625597d66d4d8876a
DavidLeDuy <davidleduy@gmail.com>
2019-05-10 15:32:48 +0200

    alles weg

--- plot_model.py
+++
@@ -1,7 +0,0 @@
-from keras.utils import plot_model
-
-from utils import load_model
-
-if __name__ == '__main__':
-    model = load_model()
-    plot_model(model, to_file='model.svg', show_layer_names=True, show_shapes=True)
